05926b577863262aafd959b99599149397e2543d
HyungWo-o <dbwotns153@naver.com>
2025-06-16 21:17:57 +0900

    응답 객체 ResponseEntity로 씌워 반환

--- src/main/java/com/example/taskflow/domain/user/controller/UserController.java
+++ src/main/java/com/example/taskflow/domain/user/controller/UserController.java
@@ -4,10 +4,13 @@ import com.example.taskflow.domain.common.dto.ApiResponse;
 import com.example.taskflow.domain.user.dto.request.DeleteUserRequest;
 import com.example.taskflow.domain.user.dto.request.LoginRequest;
 import com.example.taskflow.domain.user.dto.request.RegisterRequest;
+import com.example.taskflow.domain.user.dto.response.RegisterResponse;
 import com.example.taskflow.domain.user.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -22,23 +25,23 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/registration")
-    public ApiResponse register(@Valid @RequestBody RegisterRequest registerRequest) {
-        return userService.register(registerRequest);
+    public ResponseEntity register(@Valid @RequestBody RegisterRequest registerRequest) {
+        return ResponseEntity.ok(userService.register(registerRequest));
     }
 
     @PostMapping("/login")
-    public ApiResponse login(@Valid @RequestBody LoginRequest loginRequest) {
-        return userService.login(loginRequest);
+    public ResponseEntity login(@Valid @RequestBody LoginRequest loginRequest) {
+        return ResponseEntity.ok(userService.login(loginRequest));
     }
 
     @GetMapping("/users/profile")
-    public ApiResponse myProfile(@AuthenticationPrincipal UserDetails userDetails) {
-        return userService.myProfile(userDetails);
+    public ResponseEntity myProfile(@AuthenticationPrincipal UserDetails userDetails) {
+        return ResponseEntity.ok(userService.myProfile(userDetails));
     }
 
     @PostMapping("/users/deletion")
-    public ApiResponse deletion(@AuthenticationPrincipal UserDetails userDetails, @RequestBody DeleteUserRequest deleteUserRequest) {
-        return userService.deletion(userDetails, deleteUserRequest.getPassword());
+    public ResponseEntity deletion(@AuthenticationPrincipal UserDetails userDetails, @RequestBody DeleteUserRequest deleteUserRequest) {
+        return ResponseEntity.ok(userService.deletion(userDetails, deleteUserRequest.getPassword()));
     }
 
 }
