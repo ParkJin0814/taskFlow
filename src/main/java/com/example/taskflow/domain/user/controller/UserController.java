726f3ee9df52231f9d08a7f46a69e5b8c89ecec0
HyungWo-o <dbwotns153@naver.com>
2025-06-16 18:05:51 +0900

    응답 데이터 ApiResponse 공통 응답 처리

--- src/main/java/com/example/taskflow/domain/user/controller/UserController.java
+++ src/main/java/com/example/taskflow/domain/user/controller/UserController.java
@@ -1,7 +1,8 @@
 package com.example.taskflow.domain.user.controller;
 
+import com.example.taskflow.domain.common.dto.ApiResponse;
+import com.example.taskflow.domain.user.dto.request.LoginRequest;
 import com.example.taskflow.domain.user.dto.request.RegisterRequest;
-import com.example.taskflow.domain.user.dto.response.RegisterResponse;
 import com.example.taskflow.domain.user.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -20,8 +21,13 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/registration")
-    public RegisterResponse register(@Valid @RequestBody RegisterRequest registerRequest) {
+    public ApiResponse register(@Valid @RequestBody RegisterRequest registerRequest) {
         return userService.register(registerRequest);
+    }
+
+    @PostMapping("/login")
+    public ApiResponse login(@Valid @RequestBody LoginRequest loginRequest) {
+        return userService.login(loginRequest);
     }
 
 }
--- src/main/java/com/example/taskflow/domain/user/dto/response/RegisterResponse.java
+++ src/main/java/com/example/taskflow/domain/user/dto/response/RegisterResponse.java
@@ -1,13 +1,28 @@
 package com.example.taskflow.domain.user.dto.response;
 
+import com.example.taskflow.domain.user.enums.UserRole;
 import lombok.Getter;
+
+import java.time.LocalDateTime;
 
 @Getter
 public class RegisterResponse {
 
+    private final Long id;
+    private final String username;
+    private final String email;
+    private final String name;
+    private final UserRole role;
+    private final LocalDateTime createdAt;
     private final String bearerToken;
 
-    public RegisterResponse(String bearerToken) {
+    public RegisterResponse(Long id, String username, String email, String name, UserRole role, LocalDateTime createdAt, String bearerToken) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.name = name;
+        this.role = role;
+        this.createdAt = createdAt;
         this.bearerToken = bearerToken;
     }
 
