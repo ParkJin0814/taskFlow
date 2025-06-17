c1c60888677297727bd9342245235d375b8a5fa2
김민균 <kmg02845@gmail.com>
2025-06-17 15:01:23 +0900

    fix: import 문 삭제

--- src/main/java/com/example/taskflow/domain/common/entity/BaseEntity.java
+++ src/main/java/com/example/taskflow/domain/common/entity/BaseEntity.java
@@ -1,10 +1,7 @@
 package com.example.taskflow.domain.common.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
