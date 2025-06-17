3b52f6c43c3f878a041c3e8c7b6fbc6df8916b70
김민균 <kmg02845@gmail.com>
2025-06-17 18:44:52 +0900

    feat: CORS 설ix: add comment

--- src/main/java/com/example/taskflow/domain/auth/dto/request/RegisterRequest.java
+++ src/main/java/com/example/taskflow/domain/auth/dto/request/RegisterRequest.java
@@ -24,7 +24,7 @@ public class RegisterRequest {
     @Pattern(regexp = ".*\\d.*", message = "새 비밀번호는 숫자를 포함해야 합니다.")
     @Pattern(regexp = ".*[`~!@#$%^&*].*", message = "새 비밀번호는 특수문자를 포함해야 합니다.")
     private final String password;
-
+    // 프론트에서만 2중으로 검증하지 요청할 때 필요 치 않은 값.
 //    @NotBlank(message = "비밀번호가 일치하지 않습니다.")
 //    private final String passwordCheck;
 
