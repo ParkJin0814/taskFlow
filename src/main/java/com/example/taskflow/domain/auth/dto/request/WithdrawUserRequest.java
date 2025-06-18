e2b2f6be2c6a76f070a48b976f94dba88f3a7cf7
HyungWo-o <dbwotns153@naver.com>
2025-06-18 12:51:20 +0900

    회원탈퇴 유효성 검증 수정

--- src/main/java/com/example/taskflow/domain/auth/dto/request/WithdrawUserRequest.java
+++ src/main/java/com/example/taskflow/domain/auth/dto/request/WithdrawUserRequest.java
@@ -9,10 +9,6 @@ import lombok.Getter;
 public class WithdrawUserRequest {
 
     @NotBlank(message = "비밀번호를 입력해주세요.")
-    @Size(min = 8, message = "비밀번호는 8자 이상이어야 합니다.")
-    @Pattern(regexp = ".*[a-zA-Z].*", message = "비밀번호는 영문자를 포함해야 합니다.")
-    @Pattern(regexp = ".*\\d.*", message = "비밀번호는 숫자를 포함해야 합니다.")
-    @Pattern(regexp = ".*[`~!@#$%^&*].*", message = "비밀번호는 특수문자를 포함해야 합니다.")
     private String password;
 
 }
